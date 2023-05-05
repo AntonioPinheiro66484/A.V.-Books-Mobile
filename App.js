27218948ed0bf7deb5ec5785470e7c162ec395a6
HadeSatanist <ap66484@gmail.com>
2023-05-04 21:07:12 -0300

    adicionando componentes na pagina principal

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header';
+import Body from './src/components/Body';
+import Footer from './src/components/Footer'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header></Header>
+      <Body></Body>
+      <Footer></Footer>
     </View>
   );
 }
