01334fd066ff9e7acf6a6d1729fc3c4f8a8b85e4
HadeSatanist <ap66484@gmail.com>
2023-05-04 21:08:23 -0300

    criando componente e estilo de footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
+
+const Footer =()=>{
+    return(
+        <View>
+            <Text>Footer</Text>
+        </View>
+    )
+}
+export default Footer
