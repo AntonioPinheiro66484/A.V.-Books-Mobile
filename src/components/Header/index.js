3ed07be8f987252d4fdbdddb91a915b3abae48e4
HadeSatanist <ap66484@gmail.com>
2023-05-04 21:08:33 -0300

    criando componente e estilo de header

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
+
+const Footer =()=>{
+    return(
+        <View>
+            <Text>Footer</Text>
+        </View>
+
+    )
+}
+export default Footer
